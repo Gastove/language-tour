5338fe07584a821e51a29726a1d440cf910972ff
Ross Donaldson <gastove@gmail.com>
2017-05-22 13:16:28 -0700

    Add reference implementation for reals

---
+++ reference-impl/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to reference-impl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ reference-impl/test/reference_impl/core_test.clj
@@ -0,0 +1,7 @@
+(ns reference-impl.core-test
+  (:require [clojure.test :refer :all]
+            [reference-impl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
